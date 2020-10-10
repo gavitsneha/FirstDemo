46b5b54b37a30fa46338ff523ced499f5703a734
sneha gavit <snehag151199@gmail.com>
2020-10-11 03:15:30 +0530

    this is commit made after adding comments into the java code

--- A.java.java
+++ A.java.java
@@ -1,6 +1,6 @@
-class First{
+class First{                                     //this is starting of java class     
 public static void main(String args[])
-{
+{                                                //this is main function
 System.out.println("Hello Java");
 }
 }
